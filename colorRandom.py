383f4fd1710f547c501c0606b4db7ee855a7b814
ivoayala <ivoayala@hotmail.com>
2018-03-05 23:22:36 +0000

    colorRandom.py updated

--- colorRandom.py
+++ colorRandom.py
@@ -16,33 +16,33 @@ n3 = random.randrange(0,256)
 display = pyupm_jhd1313m1.Jhd1313m1(0,0x3E, 0x62)
 display.setColor(n1,n2,n3)
 
-sensores = pyupm_th02.TH02(6,0x40)
+#sensores = pyupm_th02.TH02(6,0x40)
 
 Switch = 0
 Temperature = 0
 Humidity = 0
 
-while True:
+#while True:
 
-    Temperature = round(sensores.getTemperature(),2)
-    Humidity = round(sensores.getHumidity(),2)
-    status = sensores.getStatus()
-    name = sensores.name()
+#    Temperature = round(sensores.getTemperature(),2)
+#    Humidity = round(sensores.getHumidity(),2)
+#    status = sensores.getStatus()
+#    name = sensores.name()
     
     
-    print 'La temperatura es: '  + str(Temperature)
-    print 'La humedad es: ' + str(Humidity) 
-    print n1 
-    print n2 
-    print n3 
+#    print 'La temperatura es: '  + str(Temperature)
+#    print 'La humedad es: ' + str(Humidity) 
+#    print n1 
+#    print n2 
+#    print n3 
    # print 'Status es :' + str(status)
    # print 'Name es :' + str(name)
-    display.setCursor(0,0)
+#    display.setCursor(0,0)
 
 
-    display.write('TEMP: ' + str(Temperature))
-    display.setCursor(1,0)
-    display.write('HUMI:  ' + str(Humidity))
-    time.sleep(1.5)
-    
+#    display.write('TEMP: ' + str(Temperature))
+#    display.setCursor(1,0)
+#    display.write('HUMI:  ' + str(Humidity))
+#    time.sleep(1.5)
 
+#exit()
